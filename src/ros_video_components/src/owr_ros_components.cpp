dab542dcadc30b78f1601b7e128f7b0690eaba54
Flora Li <floracitrus@gmail.com>
2017-05-06 13:34:45 +1000

    update  OWRS-346 #10min update registerType

--- src/ros_video_components/src/owr_ros_components.cpp
+++ src/ros_video_components/src/owr_ros_components.cpp
@@ -1,10 +1,12 @@
 #include "ros_video_components/owr_ros_components.hpp"
 #include "ros_video_components/ros_video_component.hpp"
-#include "ros_video_components/rover_cmd_state.hpp"
+#include "ros_video_components/ros_wheel_Visualize.hpp"
+//#include "ros_video_components/rover_cmd_state.hpp"
 
 #include <QQmlEngine>
 
 void OWR_ROS_Components::registerTypes(const char *uri) {
     qmlRegisterType<ROS_Video_Component>("bluesat.owr", 1, 0, "ROSVideoComponent");
-    qmlRegisterSingletonType<Rover_Cmd_State>("bluesat.owr.singleton", 1, 0, "RoverCmdState", &Rover_Cmd_State::qml_instance);
+    qmlRegisterType<ROS_Wheel_Visualize>("bluesat.owr", 1, 0, "ROSWheelVisualize");
+    //qmlRegisterSingletonType<Rover_Cmd_State>("bluesat.owr.singleton", 1, 0, "RoverCmdState", &Rover_Cmd_State::qml_instance);
 }
